f956865e69d70e720fdb435c699b76df3e667b08
Mahendra <cmrajan@gmail.com>
2020-08-12 15:35:35 -0700

    api version

--- main.go
+++ main.go
@@ -10,6 +10,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	_ "expvar"
 	"flag"
@@ -19,13 +20,18 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"runtime/pprof"
+	"strings"
 	"time"
 
 	"github.com/blevesearch/bleve"
 	bleveHttp "github.com/blevesearch/bleve/http"
 	"github.com/blevesearch/bleve/index/scorch"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
 
 var batchSize = flag.Int("batchSize", 150, "batch size for indexing")
@@ -50,9 +56,20 @@ func main() {
 		}
 		pprof.StartCPUProfile(f)
 	}
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
 
-	// open the index
-	beerIndex, err := bleve.Open(*indexPath)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	//open the index
+	venmurasuIndex, err := bleve.Open(*indexPath)
+
 	if err == bleve.ErrorIndexPathDoesNotExist {
 		log.Printf("Creating new index...")
 		// create a mapping
@@ -60,8 +77,8 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		//beerIndex, err = bleve.New(*indexPath, indexMapping)
-		beerIndex, err := bleve.NewUsing(*indexPath, indexMapping, scorch.Name, scorch.Name, map[string]interface{}{
+		//venmurasuIndex, err = bleve.New(*indexPath, indexMapping)
+		venmurasuIndex, err := bleve.NewUsing(*indexPath, indexMapping, scorch.Name, scorch.Name, map[string]interface{}{
 			"forceSegmentType":    "zap",
 			"forceSegmentVersion": 12,
 		})
@@ -72,7 +89,7 @@ func main() {
 
 		// index data in the background
 		go func() {
-			err = indexBeer(beerIndex)
+			err = indexVenmurasu(venmurasuIndex)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -93,33 +110,224 @@ func main() {
 	}
 
 	// create a router to serve static files
-	router := staticFileRouter()
 
 	// add the API
-	bleveHttp.RegisterIndexName("beer", beerIndex)
-	searchHandler := bleveHttp.NewSearchHandler("beer")
-	commonSearchHandler := bleveHttp.NewSearchHandler("beer")
+	bleveHttp.RegisterIndexName("venmurasu", venmurasuIndex)
+	searchHandler := bleveHttp.NewSearchHandler("venmurasu")
 
-	// stylesFacet := bleve.NewFacetRequest("bookname", 10)
-	// searchRequest.AddFacet("bookname", stylesFacet)
+	// router.Handle("/api/search", searchHandler).Methods("POST")
 
-	router.Handle("/api/search", searchHandler).Methods("POST")
-	router.Handle("/api/vensearch", commonSearchHandler).Methods("POST")
-	listFieldsHandler := bleveHttp.NewListFieldsHandler("beer")
-	router.Handle("/api/fields", listFieldsHandler).Methods("GET")
-
-	debugHandler := bleveHttp.NewDebugDocumentHandler("beer")
-	debugHandler.DocIDLookup = docIDLookup
-	router.Handle("/api/debug/{docID}", debugHandler).Methods("GET")
+	r.Route("/api", func(r chi.Router) {
+		r.With(searchParams).Post("/search", searchHandler.ServeHTTP) // POST /articles
+		r.Post("/stdsearch", searchHandler.ServeHTTP)
+	})
 
 	// start the HTTP server
-	http.Handle("/", router)
-	log.Printf("Listening on %v", *bindAddr)
-	log.Fatal(http.ListenAndServe(*bindAddr, nil))
+	// http.Handle("/", router)
+	// log.Printf("Listening on %v", *bindAddr)
+	// log.Fatal(http.ListenAndServe(*bindAddr, nil))
 
+	log.Printf("Listening on %s", ":8094")
+	http.ListenAndServe(":8094", r)
 }
 
-func indexBeer(i bleve.Index) error {
+// func Search(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("inside search")
+// 	venmurasuIndex, err := bleve.Open(*indexPath)
+// 	fmt.Println("after open")
+// 	if err != nil {
+// 		fmt.Println("err opening index==>", err)
+// 		panic(err)
+// 	}
+
+// 	query := bleve.NewMatchQuery("இளைய யாதவர்")
+// 	searchRequest := bleve.NewSearchRequest(query)
+// 	searchResults, err := venmurasuIndex.Search(searchRequest)
+// 	if err != nil {
+// 		fmt.Println("errerrerrerr==>", err)
+// 		panic(err)
+// 	}
+
+// 	fmt.Println(searchResults)
+
+// }
+
+type ReqJSON struct {
+	SearchQry string `json:"search"`
+	From      int    `json:"from"`
+	Size      int    `json:"size"`
+}
+
+func searchParams(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var from, size int
+		var source, tag, search, bookno, bookname string
+		var searchQry []string
+		var req ReqJSON
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&req)
+
+		if err != nil {
+			fmt.Printf("error reading request body in middleware: %v", err)
+			return
+		}
+
+		source = req.SearchQry
+		from = req.From
+		size = req.Size
+		if size == 0 {
+			size = 10
+		}
+		fmt.Println("sourcesourcesource==>", source)
+
+		if strings.Contains(source, "tags:") {
+			tagsre := regexp.MustCompile(`(?P<tags>tags:.*?\b)`)
+
+			tag = strings.Replace(tagsre.FindStringSubmatch(source)[0], "tags:", "", 1)
+
+			searchQry = append(searchQry, `{
+				"term": "`+strings.TrimRight(tag, " ")+`",
+				"field": "tags",
+				"boost": 1
+			}`)
+
+		}
+		if strings.Contains(source, "search:") {
+			searchre := regexp.MustCompile(`(?P<search>search:'(.*?)')`)
+
+			fmt.Println(searchre.FindStringSubmatch(source)[0])
+			search = strings.Replace(strings.Replace(searchre.FindStringSubmatch(source)[0], "search:", "", 1), "'", "", 2)
+
+			searchQry = append(searchQry, `{                
+                "match_phrase": "`+search+`"  ,
+				"field": "_all",
+				"boost":1
+            }`)
+		}
+		if strings.Contains(source, "bookno:") {
+			booknore := regexp.MustCompile(`(?P<bookno>bookno:.*?\b)`)
+			fmt.Println(booknore.FindStringSubmatch(source)[0])
+			bookno = strings.Replace(booknore.FindStringSubmatch(source)[0], "bookno:", "", 1)
+			searchQry = append(searchQry, `{
+				"term": "`+strings.TrimRight(bookno, " ")+`",
+				"field": "bookno",
+				"boost": 1
+			}`)
+		}
+		if strings.Contains(source, "bookname:") {
+			booknamere := regexp.MustCompile(`(?P<bookname>bookname:)`)
+			fmt.Println(booknamere.FindStringSubmatch(source)[0])
+			bookname = strings.Replace(booknamere.FindStringSubmatch(source)[0], "bookname:", "", 1)
+			searchQry = append(searchQry, `{
+				"term": "`+strings.TrimRight(bookname, " ")+`",
+				"field": "bookname",
+				"boost": 1
+			}`)
+
+		}
+		fmt.Println(tag, search, bookno, bookname)
+		searchQryFinal := strings.Join(searchQry, ",")
+
+		// {
+		// 	"query": {
+		// 	  "must": {
+		// 		"conjuncts": [
+		// 		  {
+		// 			"term": "1",
+		// 			"field": "bookno",
+		// 			"boost": 1
+		// 		  },
+		// 				  {
+		// 			"match": "ஜனமேஜயன்",
+		// 			"field": "_all",
+		// 			"boost": 1
+		// 		  }
+
+		// 		],
+		// 		"boost": 1
+		// 	  },
+		// 	  "boost": 1
+		// 	},
+		// 	"highlight": {},
+		// 	"size": 10
+		//   }
+
+		// {
+		// 	"query": {
+		// 	  "must": {
+		// 		"conjuncts": [
+		// 		  {
+		// 			"term": "1",
+		// 			"field": "bookno"
+		// 		  },
+		// 		 {
+		// 			"match": "ஜனமேஜயன்",
+		// 			"field": "_all"
+		// 		  }
+
+		// 		]
+
+		// 	  }
+
+		// 	},
+		// 	"highlight": {},
+		// 	"size": 10
+		//   }
+
+		qry :=
+			fmt.Sprintf(`{
+			"from": %d,
+			"size": %d,	
+			"query": {
+			  "must": {
+				"conjuncts": [   
+				%s
+				] 
+			  } 
+			},
+			"highlight": {}
+		  }`, from, size, searchQryFinal)
+		fmt.Println("Search query===>", qry)
+
+		formattedQry := []byte(qry)
+
+		r.Body = ioutil.NopCloser(bytes.NewReader(formattedQry))
+
+		// Here we are pssing our custom response writer to the next http handler.
+
+		// _, err := ioutil.ReadAll(r.Body)
+		// if err != nil {
+		// 	fmt.Printf("error reading request body: %v", err)
+		// 	return
+		// }
+
+		// r.Body = `{
+		// 	"query": {
+		// 	  "must": {
+		// 		"conjuncts": [
+
+		// 				  {
+		// 			"match": "ஜனமேஜயன்",
+		// 			"field": "_all",
+		// 			"boost": 1
+		// 		  }
+
+		// 		],
+		// 		"boost": 1
+		// 	  },
+		// 	  "boost": 1
+		// 	},
+		// 	"highlight": {},
+		// 	"size": 10
+		//   }`
+		// just a stub.. some ideas are to look at URL query params for something like
+		// the page number, or the limit, and send a query cursor down the chain
+		next.ServeHTTP(w, r)
+	})
+}
+
+func indexVenmurasu(i bleve.Index) error {
 
 	// open the directory
 	dirEntries, err := ioutil.ReadDir(*jsonDir)
